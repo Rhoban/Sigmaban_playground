d4900a4d02347b0c90d011f131e8bec26e8b0fe8
Grégoire Passault <g.passault@gmail.com>
2025-07-05 22:22:35 +0200

    Train script

---
+++ train.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ $# -le 0 ]
+then
+    echo "Usage: train.sh [tag]"
+    exit
+fi
+
+echo "Killing existent python"
+killall -9 python3
+
+echo "Running in background..."
+nohup uv run playground/sigmaban2024/runner.py \
+	--task flat_terrain_backlash \
+	--num_timesteps 300000000 \
+    --output_dir checkpoints/$1 \
+	--wandb \
+	> out.log &
+
+# --restore_checkpoint_path $HOME/Sigmaban_playground/checkpoints/2025_07_05_174512_171704320/ \
+
+echo "Showing log"
+tail -f out.log
