3077cc7d13b234e06b55a8a496e896416a86b07a
Gregwar <g.passault@gmail.com>
2025-07-06 11:14:19 +0200

    Sending out.log to checkpoints dir

--- train.sh
+++ train.sh
@@ -10,12 +10,13 @@ echo "Killing existent python"
 killall -9 python3
 
 echo "Running in background..."
+OUT_LOG=checkpoints/$i/out.log
 nohup uv run playground/sigmaban2024/runner.py \
 	--task flat_terrain_backlash \
 	--num_timesteps 300000000 \
-    --output_dir checkpoints/$1 \
+	--output_dir checkpoints/$1 \
 	--wandb \
-	> out.log &
+	> $OUT_LOG &
 
 # --restore_checkpoint_path $HOME/Sigmaban_playground/checkpoints/2025_07_05_174512_171704320/ \
 
