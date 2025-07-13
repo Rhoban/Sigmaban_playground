2e5ae51a6141f4430b98c832e395b12f28f36e42
apirrone <awanegenebayo@gmail.com>
2025-07-13 12:27:41 -0300

    push recovery env

--- playground/sigmaban2024/custom_rewards.py
+++ playground/sigmaban2024/custom_rewards.py
@@ -175,7 +175,7 @@ def reward_imitation(
         # + torso_orientation_rew
     )
 
-    reward *= cmd_norm > 0.01  # No reward for zero commands.
+    # reward *= cmd_norm > 0.01  # No reward for zero commands.
     return jp.nan_to_num(reward)
 
 
