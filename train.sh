357d14fa1ea3cc6e7cb557fdbb3227c9ee4b4f84
Grégoire Passault <g.passault@gmail.com>
2025-07-14 22:05:18 +0200

    Training motors backlash

--- train.sh
+++ train.sh
@@ -15,7 +15,7 @@ mkdir -p $CHECKPOINT_DIR
 OUT_LOG=$CHECKPOINT_DIR/out.log
 touch $OUT_LOG
 nohup uv run playground/sigmaban2024/runner.py \
-	--task flat_terrain_backlash \
+	--task flat_terrain_motors_backlash \
 	--num_timesteps 300000000 \
 	--output_dir $CHECKPOINT_DIR \
 	--wandb \
