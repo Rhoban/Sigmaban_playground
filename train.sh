7188efbf0ce6c94f5d42c5ec82937964fbdc800f
apirrone <awanegenebayo@gmail.com>
2025-07-15 16:47:13 -0300

    shoot

--- train.sh
+++ train.sh
@@ -15,8 +15,8 @@ mkdir -p $CHECKPOINT_DIR
 OUT_LOG=$CHECKPOINT_DIR/out.log
 touch $OUT_LOG
 nohup uv run playground/sigmaban2024/runner.py \
-	--task flat_terrain_backlash_parkour \
-	--env parkour \
+	--task flat_terrain_backlash_shoot \
+	--env shoot \
 	--num_timesteps 300000000 \
 	--output_dir $CHECKPOINT_DIR \
 	--wandb \
